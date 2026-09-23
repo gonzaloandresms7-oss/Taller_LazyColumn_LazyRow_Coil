178dda3ce00f8364549c38e208523ff119133b05
gonzaloandresms7-oss <gonzaloandresms7@gmail.com>
2026-09-23 14:31:09 -0500

    Actualmente estoy en un punto que no entiendo porque me sale error en PostCard

---
+++ app/src/main/java/me/gonzalomejia/taller_lazycolumn_lazyrow_coil/model/LazyColumnBasico.kt
@@ -0,0 +1,39 @@
+package me.gonzalomejia.taller_lazycolumn_lazyrow_coil.model
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BasicFeedList(posts: List<Post>)
+{
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(
+            items=posts,
+            key= { post -> post.id}
+        ){post -> PostCard(post=post)}
+    }
+}
+
+
+
+@Composable
+fun FeedWithIndex(posts: List<Post>){
+    LazyColumn {
+        itemsIndexed(posts){
+            index, post ->
+            Text("Post #$index: ${post.username}")
+        }
+    }
+}
